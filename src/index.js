2a253c56cabe4896eaeb78b3912f5841d242af2a
Annarchive <ytpt@mail.ru>
2022-06-28 17:57:05 +0400

    add handleClick function to Board

--- src/index.js
+++ src/index.js
@@ -22,6 +22,12 @@ class Board extends React.Component {
         };
     }
 
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
         return <Square
             value={this.state.squares[i]}

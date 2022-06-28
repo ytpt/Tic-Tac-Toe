ca668e35b73bae26c280b75067f4e5df8277d1e9
Annarchive <ytpt@mail.ru>
2022-06-28 18:29:10 +0400

    delete constructor from Board; replace this.state.squares[i] to this.props.squares[i] renderSquare method in Board; replace this.handleClick(i) to this.props.onClick(i) in renderSquare method in Board

--- src/index.js
+++ src/index.js
@@ -13,14 +13,6 @@ const Square = (props) => {
 
 class Board extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
     handleClick(i) {
         const squares = this.state.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -36,8 +28,8 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)} />
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)} />
         )
     }
 

649d5a863216b7ea54c81d1c1f255e481e8f51f7
Annarchive <ytpt@mail.ru>
2022-06-28 17:13:40 +0400

    refactor function to arrow function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="square" onClick={function() {
+            <button className="square" onClick={ () => {
                 console.log('click')}}>
                 {this.props.value}
             </button>

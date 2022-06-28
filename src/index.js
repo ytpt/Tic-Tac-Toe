3ba055b558b6842365c2eebabc6f2b339ab9e86b
Annarchive <ytpt@mail.ru>
2022-06-28 22:24:21 +0400

    update handleClick method in Game component

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -64,6 +64,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
